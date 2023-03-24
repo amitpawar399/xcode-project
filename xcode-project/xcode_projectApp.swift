4e9f453f8fd24291864e1a360fd2185f00af21f7
Amit Pawar <amitinandroid@gmail.com>
2023-03-24 21:17:46 +0530

    first commit

---
+++ xcode-project/xcode_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_projectApp.swift
+//  xcode-project
+//
+//  Created by Amit Pawar on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct xcode_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
